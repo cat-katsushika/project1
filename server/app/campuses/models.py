c7ab39b17908802ff6becef616dcacf39e6157db
pg-endokenta <k.endo@4nonome.com>
2023-07-07 20:32:24 +0900

    大学関連のモデル作成

---
+++ server/app/campuses/models.py
@@ -0,0 +1,13 @@
+from uuid import uuid4
+
+from django.db import models
+
+
+class University(models.Model):
+    id = models.UUIDField(default=uuid4, primary_key=True, editable=False)
+    name = models.CharField(max_length=100)
+
+class Campus(models.Model):
+    id = models.UUIDField(default=uuid4, primary_key=True, editable=False)
+    university = models.ForeignKey(University, on_delete=models.CASCADE)
+    campus = models.CharField(max_length=100)
