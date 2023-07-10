8665f0df392de68cbae0f06b3a5947d00077fe3c
harune-pg <h.makino.22.6.533@4nonome.com>
2023-07-10 11:08:31 +0900

    コンフリクトを解消

--- server/app/config/settings.py
+++ server/app/config/settings.py
@@ -121,12 +121,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
-<<<<<<< HEAD
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 MEDIA_URL = "/media/"
-=======
->>>>>>> dev
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
