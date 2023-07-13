9d8f67b7a36d2abf7e83c6af0f47c1ed53fe1cf6
harune-pg <h.makino.22.6.533@4nonome.com>
2023-07-14 02:11:49 +0900

    出品モデルを修正

--- server/app/items/models.py
+++ server/app/items/models.py
@@ -7,9 +7,6 @@ from campuses.models import Campus
 
 
 class Item(models.Model):
-    class Meta:
-        verbose_name = "出品"
-
     class ListingStatus(models.TextChoices):
         UNPURCHASED = "unpurchased", "未購入"
         PURCHASED = "purchased", "購入済み"
@@ -27,24 +24,24 @@ class Item(models.Model):
         LITTLE = "little", "少しある"
         LOT = "lot", "かなりある"
 
-    seller = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="sells")
-    buyer = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="buys")
-    listing_stauts = models.CharField(max_length=30, choices=ListingStatus.choices, default=ListingStatus.UNPURCHASED)
-    price = models.PositiveIntegerField()
+    seller = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="sell_item")
+    buyer = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, blank=True, related_name="buy_item")
+    listing_status = models.CharField(max_length=11, choices=ListingStatus.choices, default=ListingStatus.UNPURCHASED)
+    price = models.PositiveSmallIntegerField()
     name = models.CharField(max_length=50)
     description = models.CharField(max_length=255)
-    condition = models.CharField(max_length=30, choices=Condition.choices)
-    writing_state = models.CharField(max_length=30, choices=WritingState.choices)
-    receivable_campus = models.ForeignKey(Campus, on_delete=models.CASCADE)
+    condition = models.CharField(max_length=7, choices=Condition.choices)
+    writing_state = models.CharField(max_length=6, choices=WritingState.choices)
+    receivable_campus = models.ForeignKey(Campus, on_delete=models.PROTECT)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.name
 
 
-# アップロード先のパス: <MEDIA_ROOT>/images/item/<出品者のユーザーID>/<ファイル名>
+# アップロード先のパス: <MEDIA_ROOT>/images/items/<出品者のユーザーID>/<ファイル名>
 def get_item_image_path(instance, filename):
-    return "images/item/{0}/{1}".format(instance.parent_item.seller.id, filename)
+    return "images/items/{0}/{1}".format(instance.parent_item.seller.id, filename)
 
 
 class Image(models.Model):
