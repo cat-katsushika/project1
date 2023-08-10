39054980f9d4465fd76901077c90ae55d3d15533
pg-endokenta <k.endo@4nonome.com>
2023-08-11 05:50:52 +0900

    apiの実装

--- server/app/comments/models.py
+++ server/app/comments/models.py
@@ -4,9 +4,15 @@ from django.conf import settings
 from django.db import models
 from django.utils import timezone
 
+from items.models import Item
+
 
 class Comment(models.Model):
     id = models.UUIDField(default=uuid4, primary_key=True, editable=False)
+    item_id = models.ForeignKey(Item, on_delete=models.CASCADE)
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     comment = models.CharField(max_length=100)
     created_at = models.DateTimeField(default=timezone.now)
+
+    def __str__(self) -> str:
+        return self.comment
