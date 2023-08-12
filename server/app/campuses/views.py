646cf44d161af695d123cc52006b511f93dcf205
pg-endokenta <k.endo@4nonome.com>
2023-08-12 21:53:06 +0900

    大学名による検索機能追加

--- server/app/campuses/views.py
+++ server/app/campuses/views.py
@@ -1,23 +1,30 @@
-from rest_framework.generics import ListAPIView, RetrieveAPIView
+from rest_framework import generics
+from rest_framework.response import Response
+from rest_framework.views import APIView
 from .models import University, Campus
 from .serializers import UniversitySerializer, CampusSerializer
 
 
-class UniversityDetailView(RetrieveAPIView):
+class UniversityDetailView(generics.RetrieveAPIView):
     queryset = University.objects.all()
     serializer_class = UniversitySerializer
 
 
-class UniversityListView(ListAPIView):
-    queryset = University.objects.all()
-    serializer_class = UniversitySerializer
+class UniversityListView(APIView):
+    def get(self, request, *args, **kwargs):
+        queryset = University.objects.all()
+        name = request.query_params.get("name", None)
+        if name:
+            queryset = queryset.filter(name__icontains=name)
+        serializer = UniversitySerializer(queryset, many=True)
+        return Response(serializer.data)
 
 
-class CampusDetailView(RetrieveAPIView):
+class CampusDetailView(generics.RetrieveAPIView):
     queryset = Campus.objects.all()
     serializer_class = CampusSerializer
 
 
-class CampusListView(ListAPIView):
+class CampusListView(generics.ListAPIView):
     queryset = Campus.objects.all()
     serializer_class = CampusSerializer
