bc2404b3018ab6bfb87978912cc2d07b0a79ee33
khirooka <l.00.kocha.00.l@gmail.com>
2023-08-18 16:09:27 +0900

    add: emailバリデーション

--- server/app/accounts/serializers.py
+++ server/app/accounts/serializers.py
@@ -1,18 +1,13 @@
 from django.contrib.auth import get_user_model
+from djoser.serializers import UserCreatePasswordRetypeSerializer
 from rest_framework import serializers
 
 User = get_user_model()
 
 
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = [
-            "id",
-            "email",
-        ]
-
+class UserSerializer(UserCreatePasswordRetypeSerializer):
     def validate_email(self, value):
         if not value.endswith("@ed.tus.ac.jp"):
             raise serializers.ValidationError("メールアドレスの形式は「@ed.tus.ac.jp」です")
+
         return value
