5efd9fe736771789175ff781f2a21eb9787b5088
khirooka <l.00.kocha.00.l@gmail.com>
2023-08-20 20:15:52 +0900

    add: djsoer, login logout用のpath

--- server/app/config/settings.py
+++ server/app/config/settings.py
@@ -145,6 +145,7 @@ REST_FRAMEWORK = {
         "rest_framework.permissions.IsAuthenticated",
     ],
     "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.TokenAuthentication",
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ],
 }
