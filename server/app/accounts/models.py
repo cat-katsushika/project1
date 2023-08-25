58f56f77b196a80afe8a4333cf9194130d33527d
khirooka <l.00.kocha.00.l@gmail.com>
2023-08-26 04:54:35 +0900

    fix: create_superuserでis_activeをtrueに設定

--- server/app/accounts/models.py
+++ server/app/accounts/models.py
@@ -22,6 +22,7 @@ class UserManager(BaseUserManager):
         user = self.create_user(email, password, **extra_fields)
         user.is_superuser = True
         user.is_staff = True
+        user.is_active = True
         user.save()
 
         return user
