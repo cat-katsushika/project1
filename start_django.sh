f2b5fb3a745848913fac1c63da358b0a17c715e4
khirooka <l.00.kocha.00.l@gmail.com>
2023-08-27 00:50:48 +0900

    fix: loaddata

--- start_django.sh
+++ start_django.sh
@@ -16,7 +16,7 @@ pipenv run python manage.py makemigrations
 pipenv run python manage.py migrate
 
 # テストデータの作成
-pipenv run python manage.py loaddata */fixtures/*.josn
+pipenv run python manage.py loaddata accounts/fixtures/data.json
 
 # Djangoの開発サーバーの起動
 pipenv run python manage.py runserver
