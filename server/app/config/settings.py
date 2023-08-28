ed44b40f6bc5053cd6db990d09d5c90b73ca851a
harune-pg <h.makino.22.6.533@4nonome.com>
2023-08-28 19:07:38 +0900

    デフォルトのパーミッションにOrReadOnlyを追加

--- server/app/config/settings.py
+++ server/app/config/settings.py
@@ -148,7 +148,7 @@ AUTH_USER_MODEL = "accounts.User"
 
 REST_FRAMEWORK = {
     "DEFAULT_PERMISSION_CLASSES": [
-        "rest_framework.permissions.IsAuthenticated",
+        "rest_framework.permissions.IsAuthenticatedOrReadOnly",
     ],
     "DEFAULT_AUTHENTICATION_CLASSES": [
         "accounts.authentication.CookieJWTAuthentication",
