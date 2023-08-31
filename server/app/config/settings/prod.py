0367e24d08c20f2f6959e889d8d4350368dab130
pg-endokenta <k.endo@4nonome.com>
2023-08-31 21:43:10 +0900

    開発用と本番用でsettingsファイルを分ける

---
+++ server/app/config/settings/prod.py
@@ -0,0 +1,9 @@
+from .base import *
+
+DEBUG = False
+
+STATIC_URL = "static/"
+STATIC_ROOT = "/usr/share/nginx/html/static"  # 静的ファイルを集める場所（STATIC_ROOT）を指定
+
+MEDIA_URL = "media/"
+MEDIA_ROOT = "/usr/share/nginx/html/media"
