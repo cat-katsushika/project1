ba5d8ce8ce3b1b1db33a509c4eb6db8d09e05b41
pg-endokenta <k.endo@4nonome.com>
2023-09-01 21:59:34 +0900

    パスを修正

--- server/app/config/settings/base.py
+++ server/app/config/settings/base.py
@@ -17,7 +17,7 @@ from pathlib import Path
 import environ
 
 env = environ.Env()
-env.read_env(".env.sample")
+env.read_env("../.env.sample")
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
