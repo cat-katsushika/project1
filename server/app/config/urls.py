4a67409210b6bb1ecf3ce2cf21f5649a2948af43
harune-pg <h.makino.22.6.533@4nonome.com>
2023-09-02 18:29:03 +0900

    本番環境に向けてurls.pyを調整

--- server/app/config/urls.py
+++ server/app/config/urls.py
@@ -2,11 +2,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
-from drf_spectacular.views import (
-    SpectacularAPIView,
-    SpectacularRedocView,
-    SpectacularSwaggerView,
-)
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -21,6 +17,12 @@ urlpatterns = [
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
+    from drf_spectacular.views import (
+        SpectacularAPIView,
+        SpectacularRedocView,
+        SpectacularSwaggerView,
+    )
+
     urlpatterns += [
         path("api/schema/", SpectacularAPIView.as_view(), name="schema"),  # 追加
         path("api/schema/swagger-ui/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),  # 追加
