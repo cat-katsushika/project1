10446b4fb2662a6912671d85492e5e7363ac76d8
harune-pg <h.makino.22.6.533@4nonome.com>
2023-09-02 18:19:22 +0900

    .envファイルの読み込みパスを変更

--- server/app/config/settings/base.py
+++ server/app/config/settings/base.py
@@ -17,11 +17,12 @@ from pathlib import Path
 import environ
 
 env = environ.Env()
-env.read_env("../.env.sample")
+# env.read_env("../.env.sample")
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
+env.read_env(os.path.join(BASE_DIR, ".env"))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
