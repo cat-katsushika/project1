bd37efcec664ffe11bfbc246c5913287acf2497b
harune-pg <h.makino.22.6.533@4nonome.com>
2023-09-06 22:17:33 +0900

    ALLOWED_HOSTSを修正

--- server/app/config/settings/prod.py
+++ server/app/config/settings/prod.py
@@ -2,7 +2,7 @@ from .base import *
 
 DEBUG = False
 
-ALLOWED_HOSTS = ["160.251.10.52"]
+ALLOWED_HOSTS = ["uni-bo.net", "www.uni-bo.net"]
 
 STATIC_URL = "static/"
 STATIC_ROOT = "/usr/share/nginx/html/static"  # 静的ファイルを集める場所（STATIC_ROOT）を指定
