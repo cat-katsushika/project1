b3178758f1888b9b90ef594ea990e32ba994f363
pg-endokenta <k.endo@4nonome.com>
2023-10-01 21:13:22 +0900

    update api

--- server/app/comments/serializers.py
+++ server/app/comments/serializers.py
@@ -3,11 +3,23 @@ from .models import Comment
 
 
 class CommentSerializer(serializers.ModelSerializer):
+    user_email = serializers.SerializerMethodField()
+
     class Meta:
         model = Comment
-        fields = "__all__"
+        fields = (
+            "id",
+            "item_id",
+            "user",
+            "user_email",
+            "comment",
+            "created_at",
+        )
         read_only_fields = [
             "id",
             "created_at",
             "user",
         ]
+
+    def get_user_email(self, obj):
+        return obj.user.email
