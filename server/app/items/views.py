1190d07b80000c6a586e3d6ec76d56c8b9bdf749
pg-endokenta <k.endo@4nonome.com>
2023-10-20 20:04:03 +0900

    update items

--- server/app/items/views.py
+++ server/app/items/views.py
@@ -1,9 +1,10 @@
 from rest_framework import generics, permissions, status, views
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from .models import Item, Like
-from .serializers import ItemSerializer
+from .serializers import ItemSerializer, ItemCreateSerializer
 
 
 class ItemListPagination(PageNumberPagination):
@@ -11,7 +12,7 @@ class ItemListPagination(PageNumberPagination):
     max_page_size = 100
 
 
-class ItemListCreateView(generics.ListCreateAPIView):
+class ItemListView(generics.ListAPIView):
     queryset = Item.objects.all().order_by("-updated_at")
     serializer_class = ItemSerializer
     pagination_class = ItemListPagination
@@ -27,6 +28,55 @@ class ItemListCreateView(generics.ListCreateAPIView):
         return queryset
 
 
+class ItemCreateAPIView(APIView):
+    def post(self, request, *args, **kwargs):
+        # request.dataを変更可能な辞書にコピー
+        data = dict(request.data.lists())
+        # Itemの作成に必要なキー
+        keys_to_check = [
+            "price",
+            "name",
+            "description",
+            "condition",
+            "writing_state",
+            "receivable_campus",
+        ]
+        # キーが辞書に存在しないときエラーを返す
+        for key in keys_to_check:
+            if key not in data:
+                return Response({"error": f"{key} が必要です"}, status=status.HTTP_400_BAD_REQUEST)
+
+        data["seller"] = request.user.id
+        data["listing_status"] = Item.ListingStatus.UNPURCHASED
+        data["price"] = data["price"][0]
+        data["name"] = data["name"][0]
+        data["description"] = data["description"][0]
+        data["condition"] = data["condition"][0]
+        data["writing_state"] = data["writing_state"][0]
+        data["receivable_campus"] = data["receivable_campus"][0]
+
+        if "image_1" not in data:
+            return Response({"error": "写真が必須です。"}, status=status.HTTP_400_BAD_REQUEST)
+        data["images"] = []
+        data["images"].append({"photo_path": data.pop("image_1")[0], "order": 1})
+        # 10枚まで登録できるようにする
+        for i in range(2, 11):
+            if f"image_{i}" in data:
+                data["images"].append({"photo_path": data.pop(f"image_{i}")[0], "order": i})
+            else:
+                break
+
+        serializer = ItemCreateSerializer(data=data)
+
+        if serializer.is_valid():
+            serializer.save()
+
+            saved_item = Item.objects.get(id=serializer.data["id"])
+            response_serializer = ItemSerializer(saved_item, context={"request": request})
+            return Response(response_serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
 class IsOwnerOrAdminOrReadOnly(permissions.BasePermission):
     message = "出品者しか編集できません。"
 
@@ -40,6 +90,68 @@ class ItemRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Item.objects.all()
     serializer_class = ItemSerializer
     permission_classes = [IsOwnerOrAdminOrReadOnly]
+
+    def update(self, request, *args, **kwargs):
+        partial = self.request.query_params.get("partial", False)
+        instance = self.get_object()
+        print("partial: ", partial)
+        print("keywargs: ", kwargs)
+
+        if instance.listing_status != Item.ListingStatus.UNPURCHASED:
+            return Response({"error": "売り切れた商品は編集できません。"}, status=status.HTTP_400_BAD_REQUEST)
+
+        # request.dataを変更可能な辞書にコピー
+        data = dict(request.data.lists())
+        # Itemの作成に必要なキー
+        keys_to_check = [
+            "price",
+            "name",
+            "description",
+            "condition",
+            "writing_state",
+            "receivable_campus",
+        ]
+        # キーが辞書に存在しないときエラーを返す
+        for key in keys_to_check:
+            if key not in data:
+                if partial:
+                    continue
+                else:
+                    return Response({"error": f"{key} が必要です"}, status=status.HTTP_400_BAD_REQUEST)
+            data[key] = data[key][0]
+
+        data["seller"] = request.user.id
+        data["listing_status"] = instance.listing_status
+
+        # 以下画像の処理
+        data["images"] = []
+        # partial == True のとき
+        if partial:
+            for i in range(1, 11):
+                if f"image_{i}" in data:
+                    data["images"].append({"photo_path": data.pop(f"image_{i}")[0], "order": i})
+
+        # partial == False のとき
+        else:
+            if "image_1" not in data:
+                return Response({"error": "写真が必須です。"}, status=status.HTTP_400_BAD_REQUEST)
+            data["images"] = []
+            data["images"].append({"photo_path": data.pop("image_1")[0], "order": 1})
+            # 10枚まで登録できるようにする
+            for i in range(2, 11):
+                if f"image_{i}" in data:
+                    data["images"].append({"photo_path": data.pop(f"image_{i}")[0], "order": i})
+                else:
+                    break
+
+        serializer = ItemCreateSerializer(instance, data=data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        print("aaa")
+        self.perform_update(serializer)
+
+        updated_item = self.get_object()
+        response_serializer = ItemSerializer(updated_item, context={"request": request})
+        return Response(response_serializer.data)
 
 
 class ItemPurchaseView(generics.UpdateAPIView):
